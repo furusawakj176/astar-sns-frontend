dbb17c3064cc20654feda13543091c4fd4ea942f
furusawakj176 <je_10042000@yahoo.co.jp>
2022-12-08 15:21:28 +0900

    Section2 Lesson1 完了

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "unsplash.com",
+      "plus.unsplash.com",
+      "ibb.co",
+      "i.ibb.co", 
+      "*.ic0.app",
+    ],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
